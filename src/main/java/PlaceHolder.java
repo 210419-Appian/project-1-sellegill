79bd6f0d318d7deb943427bc710a13b94b7fca7f
Selle Gill <savannahsellegill@Savannahs-Air.zyxel.com>
2021-04-27 11:20:52 -0500

    testing git

--- src/main/java/PlaceHolder.java
+++ src/main/java/PlaceHolder.java
@@ -1,8 +1,9 @@
 
 public interface PlaceHolder {
 	
-	/*This is simply a placeholder interface so that Git will retain the project structure when 
-	 * pushed to the repo. You man immediately delete it as it is of no use for your project.  
+	/*Just attempting to push to git :)
+	 * 
+	 *  You man immediately delete it as it is of no use for your project.  
 	 */
 
 }
